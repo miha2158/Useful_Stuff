9135e366358b333f20bf33b018352a5177e5003c
Mikhail <miha.vanya@gmail.com>
2018-03-15 01:04:39 +0500

    Person keeps all lines from name files

--- Generator/Person.cs
+++ Generator/Person.cs
@@ -24,10 +24,10 @@ namespace Generator
             this.Last = Last;
         }
 
-        public static List<string> MaleFirstNames = new List<string>(0);
-        public static List<string> MaleLastNames = new List<string>(0);
-        public static List<string> FemaleFirstNames = new List<string>(0);
-        public static List<string> FemaleLastNames = new List<string>(0);
+        public static readonly List<string> MaleFirstNames = new List<string>(File.ReadAllLines(@"malefirstnames.txt"));
+        public static readonly List<string> MaleLastNames = new List<string>(File.ReadAllLines(@"malelastnames.txt"));
+        public static readonly List<string> FemaleFirstNames = new List<string>(File.ReadAllLines(@"femalefirstnames.txt"));
+        public static readonly List<string> FemaleLastNames = new List<string>(File.ReadAllLines(@"femalelastnames.txt"));
 
         public static Person FillBlanks() => FillBlanks((Gender) NewValue.Int(2));
         public static Person FillBlanks(Gender PersonGender)
@@ -37,24 +37,14 @@ namespace Generator
             {
                 case Gender.Male:
                 {
-                    if (MaleFirstNames.Count == 0)
-                        MaleFirstNames = File.ReadAllLines(@"malefirstnames.txt").ToList();
                     p.First = MaleFirstNames[NewValue.Int(MaleFirstNames.Count)];
-
-                    if (MaleLastNames.Count == 0)
-                        MaleLastNames = File.ReadAllLines(@"malelastnames.txt").ToList();
                     p.Last = MaleLastNames[NewValue.Int(MaleLastNames.Count)];
                     break;
                 }
 
                 case Gender.Female:
                 {
-                    if (FemaleFirstNames.Count == 0)
-                        FemaleFirstNames = File.ReadAllLines(@"femalefirstnames.txt").ToList();
                     p.First = FemaleFirstNames[NewValue.Int(FemaleFirstNames.Count)];
-
-                    if (FemaleLastNames.Count == 0)
-                        FemaleLastNames = File.ReadAllLines(@"femalelastnames.txt").ToList();
                     p.Last = FemaleLastNames[NewValue.Int(FemaleLastNames.Count)];
                     break;
                 }
@@ -72,8 +62,5 @@ namespace Generator
                    First == person.First &&
                    Last == person.Last;
         }
-
-        public static bool operator ==(Person a, object b) => a != null && a.Equals(b);
-        public static bool operator !=(Person a, object b) => !(a == b);
     }
 }
